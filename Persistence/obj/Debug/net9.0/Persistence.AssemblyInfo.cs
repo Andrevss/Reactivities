ac34a25349f3a9d21ae55348818db875f2176b94
André Silva <andre_vss@outlook.com>
2025-04-30 22:43:59 -0300

    Fixing the nuget and adding the entity design and sqlite

--- Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net9.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b93a4bb7173009466268f613d49e1bd7d86c3e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19c1a78051c4e58a64d5502b83702e293521901")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
