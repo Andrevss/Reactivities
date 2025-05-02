479a12b3e7c958a9d5b98a70cc273e6d83e85cd6
André Silva <andre_vss@outlook.com>
2025-05-02 16:20:34 -0300

    Creating the controllers and adding the get protocol

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3b698031ba93368c6c72709bb5193521eb93a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+caab2dda691b0dbd2b21b59b883c2a62d199b22a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
