8e3b698031ba93368c6c72709bb5193521eb93a0
André Silva <andre_vss@outlook.com>
2025-05-02 12:27:27 -0300

    Creating the migrations

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e19c1a78051c4e58a64d5502b83702e293521901")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac34a25349f3a9d21ae55348818db875f2176b94")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
